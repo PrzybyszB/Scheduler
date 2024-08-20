512ba9a709fb095cffc3332a17d09f6afbf94324
Bartek <bartass97@gmail.com>
2024-08-20 19:32:07 +0200

    Progressing home page, trying to add fade components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import Home from './pages/Home';
-import Route16 from './pages/Route16';
-
+import Home from './pages/Home/index';
+import Route16 from './pages/Routes/index';
+import Test from "./pages/Test";
 
 
 
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="16" element={<Route16 />} />
+          <Route path="test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </>
@@ -20,46 +21,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-// function App() {
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     // Sprawdź poprawność URL-a
-//     axios.get('http://localhost:8000/api/test-16/')
-//       .then(response => {
-//         // Sprawdź, czy odpowiedź zawiera dane
-//         console.log(response.data);
-//         setData(response.data);
-//       })
-//       .catch(error => {
-//         console.error('There was an error fetching the data!', error);
-//       });
-//   }, []); // Pusty array [] jako drugi argument powoduje, że useEffect uruchamia się tylko raz
-
-//   return (
-//     <div>
-//       <h1>Jak Pojadę</h1>
-//       {data ? (
-//         <div>
-//           <p>Route ID: {data.route_id}</p>
-//           <p>Route Short Name: {data.route_short_name}</p>
-//           <p>Route Long Name: {data.route_long_name}</p>
-//           <p>Route Description: {data.route_desc}</p>
-//           <p>Route Type: {data.route_type}</p>
-//           <p>Route Color: #{data.route_color}</p>
-//           <p>Route Text Color: #{data.route_text_color}</p>
-//         </div>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
--- frontend/src/pages/Home/Components/Digitalclock/Digitalclock.jsx
+++ frontend/src/pages/Home/Components/Digitalclock/Digitalclock.jsx
@@ -1,50 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import '/src/styles/home.css'
+import '/src/pages/Home/Components/Digitalclock/Digitalclock.css';
 
-function Home(){
-
-    const [time,setTime] = useState(new Date());
+export default function DigitalClock() {
+    const [time, setTime] = useState(new Date());
 
     useEffect(() => {
-        const intervalID = setInterval(() =>{
+        const intervalID = setInterval(() => {
             setTime(new Date());
-        },1000);
-
+        }, 1000);
         return () => {
             clearInterval(intervalID);
-        }
+        };
     }, []);
 
-    function formatTime(){
+    function formatTime() {
         let hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
-        const meridiem = hours >= 12 ? "PM" : "AM"
+        // const meridiem = hours >= 12 ? "PM" : "AM";
 
-        hours = hours % 12 || 12;
+        // hours = hours % 12 || 12;
 
-        return `${padZero(hours)}:${padZero((minutes))}:${padZero(seconds)}:${meridiem}`;
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
     }
 
-    function padZero(number){
-        return (number < 10 ? "0" : "") + number
+    function padZero(number) {
+        return (number < 10 ? '0' : '') + number;
     }
 
-    return(
-        <>
+    return (
         <div className='body-container'>
             <div className='clock-container'>
                 <div className='clock'>
                     <span className='clock-span'>{formatTime()}</span>
-
                 </div>
-
-
             </div>
         </div>
-        </>
     );
 }
-
-
-export default Home;
